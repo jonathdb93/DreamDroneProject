2ed0fcc6646a9e0e7f529796ca71e11ebc451a18
Jonathan De Badrihaye <jonathan.debadrihaye@gmail.com>
2018-05-17 19:57:14 +0000

    Added movement functions

--- index.js
+++ index.js
@@ -2,38 +2,42 @@ const express = require('express')
 const Drone = require('parrot-minidrone');
 
 const app = express()
-const drone = new Drone({autoconnect: true});
-
-/*
-drone.on('connected', () => {
-	drone.takeOff();
-
-
-	setTimeout(()=> {
-		drone.animate('flipFront');
-		drone.setFlightParams({
-			yaw: 30
-		});
-		drone.land();
-		drone.animate('flipBack');
-	},5000)
-});
-
-drone.on('flightStatusChange', (status) => {
-	console.log("output: " + status);
-    if (status === 'hovering') {
-//        drone.land();
-//        process.exit();
-    }
-});
-*/
+const drone = new Drone({autoconnect: true, droneFilter: 'Mambo_743291'});
 
 
 app.get('/turn/:speed', function (req, res) {
   console.log(req.query);
-  res.send("speed is set to " + req.params.speed);
+  res.send("yaw speed is set to " + req.params.speed);
   drone.setFlightParams({
             yaw: req.params.speed
+  });
+
+});
+
+
+app.get('/moveLeftRight/:speed', function (req, res) {
+  console.log(req.query);
+  res.send("roll speed is set to " + req.params.speed);
+  drone.setFlightParams({
+            roll: req.params.speed
+  });
+
+});
+
+app.get('/moveForwardBackward/:speed', function (req, res) {
+  console.log(req.query);
+  res.send("pitch speed is set to " + req.params.speed);
+  drone.setFlightParams({
+            pitch: req.params.speed
+  });
+
+});
+
+app.get('/moveUpDown/:speed', function (req, res) {
+  console.log(req.query);
+  res.send("altitude speed is set to " + req.params.speed);
+  drone.setFlightParams({
+            altitude: req.params.speed
   });
 
 });
@@ -49,6 +53,20 @@ app.get('/land', function (req, res) {
   res.send("land");
   drone.land();
 });
+
+app.get('/emergency', function (req, res) {
+  console.log(req.params)
+  res.send("emergency");
+  drone.emergency();
+});
+
+app.get('/trim', function (req, res) {
+  console.log(req.params)
+  res.send("emergency");
+  drone.trim();
+});
+
+
 
 
 app.listen(3000)
