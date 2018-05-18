1eb05819c5b2466a1b0cff3b3a4a9066e4d32c93
Jonathan De Badrihaye <jonathan.debadrihaye@gmail.com>
2018-05-18 12:54:02 +0000

    Added some functionalities and checks

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const Drone = require('parrot-minidrone');
 
 const app = express()
-const drone = new Drone({autoconnect: true});
-
-
+const drone = new Drone({autoconnect: true, droneFilter: 'Mambo_617985'});
 
 app.get('/start', function (req, res) {
   drone.takeOff();
@@ -25,29 +23,49 @@ app.get('/trim', function (req, res) {
 
 
 
-app.get('/turn/:speed', function (req, res) {
-  drone.setFlightParams({yaw: req.params.speed});
+app.get('/turn/:speed/:duration', function (req, res) {
+	console.log('Turn - Speed: (' + req.params.speed + ') - Duration: (' + req.params.duration + ')');
+	if(!req.params.speed || !req.params.duration){
+		throw new Exception('Speed or duration is not filled in');
+	}
+	drone.setFlightParams({yaw: req.params.speed});
+	setTimeout(() => {
+  		drone.setFlightParams({yaw: 0});
+	}, req.params.duration);
 });
 
-app.get('/left/:speed', function (req, res) {
-  drone.setFlightParams({roll: req.params.speed});
+app.get('/left/:speed/:duration', function (req, res) {
+	console.log('Left - Speed: (' + req.params.speed + ') - Duration: (' + req.params.duration + ')');
+	if(!req.params.speed || !req.params.duration){
+		throw new Exception('Speed or duration is not filled in');
+	}
+  	drone.setFlightParams({roll: req.params.speed});
+  	setTimeout(() => {
+  		drone.setFlightParams({roll: 0});
+	}, req.params.duration);
 });
 
-app.get('/forward/:speed', function (req, res) {
-  drone.setFlightParams({pitch: req.params.speed});
+app.get('/forward/:speed/:duration', function (req, res) {
+	console.log('Forward - Speed: (' + req.params.speed + ') - Duration: (' + req.params.duration + ')');
+	if(!req.params.speed || !req.params.duration){
+		throw new Exception('Speed or duration is not filled in');
+	}
+	drone.setFlightParams({pitch: req.params.speed});
+	setTimeout(() => {
+		drone.setFlightParams({pitch: 0});
+	}, req.params.duration);
 });
 
 app.get('/updown/:speed', function (req, res) {
-  drone.setFlightParams({altitude: req.params.speed});
+	console.log('Updown - Speed: (' + req.params.speed + ')');
+	if(!req.params.speed){
+		throw new Exception('Speed is not filled in');
+	}
+	drone.setFlightParams({altitude: req.params.speed});
 
 });
 
-app.get('/batteryLevel', function (req, res) {
-  return drone.getBatteryLevel();
-});
-
-
-app.use(express.static('frontend/dist'));
+app.use(express.static('frontend/dist/dreamdrone'));
 
 
 app.listen(8080)
