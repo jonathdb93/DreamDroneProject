066e29e0874cd95ecd338d06cd749f7f6f5d1cf7
Arno Dhondt <arno.dhondt@cegeka.com>
2018-05-18 15:38:06 +0200

    Set correct start page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from 'src/app/app.component';
 import { DeviceControllerComponent } from 'src/app/device-controller/device-controller/device-controller.component';
 
 const routes: Routes = [
-  {path: 'home' , component: AppComponent },
+  {path: 'home' , component: DeviceControllerComponent },
   {path: 'device', component: DeviceControllerComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 { path: '**', redirectTo: '/home' }
